29776691d1e2ef49e7f8e6a8010d576034e5b110
Huynh Gia Diem Ngoc <ngochuynh150905@gmail.com>
2026-03-27 12:08:41 +0700

    search annoucement

--- UTE_Forum/src/main/java/nvt/vn/ute_forum/repository/AnnouncementRepo.java
+++ UTE_Forum/src/main/java/nvt/vn/ute_forum/repository/AnnouncementRepo.java
@@ -1,12 +1,21 @@
 package nvt.vn.ute_forum.repository;
 
 import nvt.vn.ute_forum.model.Announcement;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
 public interface AnnouncementRepo extends JpaRepository<Announcement, String>,
-        JpaSpecificationExecutor<Announcement> { // Kế thừa thêm ở đây
+        JpaSpecificationExecutor<Announcement> {
+    @Query("SELECT a FROM Announcement a WHERE " +
+            "LOWER(a.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(CAST(a.content AS string)) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Announcement> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
